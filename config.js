76893417a287fd1e2aa067ce4ff117d4ae83f94c
Pedro Fernandez <pfernandez@hplaya.local>
2026-06-19 10:51:44 +0200

    Fork sync

---
+++ config.js
@@ -0,0 +1,14 @@
+const SARAPHONE_CONFIG = {
+  login: "55001",
+  passwd: "56X5wjYXj7InhArM$3f",
+  yourname: "Senator Agent",
+  domain: "contactcenter.it.senator.tools",
+  proxy: "contactcenter.it.senator.tools",
+  port: "8089/wss",
+  pres1: "",
+  pres1_label: "",
+  pres2: "",
+  pres2_label: "",
+  pres3: "",
+  pres3_label: "",
+};
